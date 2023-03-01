624b3820487244b539fbab1614609b40c2d6a150
trustonlyyou <trustonlyyou@naver.com>
2023-03-01 16:45:28 +0900

    Bad Request 처리 @Valid, input value is Empty

--- src/main/java/me/whiteship/restapi/events/EventController.java
+++ src/main/java/me/whiteship/restapi/events/EventController.java
@@ -2,12 +2,15 @@ package me.whiteship.restapi.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -26,7 +29,12 @@ public class EventController {
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
         // Model Mapper 를 이용해서 Dto -> Entity 로 변환 시켜 줄 것이다.
         // 이때 reflection 이 발생 -> 하지만 reflection 도 JAVA 버전이 올라가면서 성능이 좋아 질것이라 크게 우려는 안함
         Event event = modelMapper.map(eventDto, Event.class);
--- src/main/java/me/whiteship/restapi/events/EventDto.java
+++ src/main/java/me/whiteship/restapi/events/EventDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder
@@ -14,14 +17,23 @@ public class EventDto {
     // Entity 에 어노테이션이 너무 많아서 햇갈릴 수 있다. -> 분산을 위해서 입력 값을 받는 DTO 를 다로 뺐다.
     // 단점은 중복이 생긴다.
 
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NotNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
     private LocalDateTime beginEventDateTime;
+    @NotNull
     private LocalDateTime endEventDateTime;
     private String location; // (optional) 이게 없으면 온라인 모임
+    @Min(0)
     private int basePrice; // (optional)
+    @Min(0)
     private int maxPrice; // (optional)
+    @Min(0)
     private int limitOfEnrollment;
 }
