d0369f56cf188b82e67488249670a611203c50e2
trustonlyyou <trustonlyyou@naver.com>
2023-03-01 16:57:40 +0900

    Bad Request 처리 Validator

--- src/test/java/me/whiteship/restapi/events/EventControllerTests.java
+++ src/test/java/me/whiteship/restapi/events/EventControllerTests.java
@@ -102,4 +102,26 @@ public class EventControllerTests {
                     )
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,26,14,24))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023,11,25,22,37))
+                .beginEventDateTime(LocalDateTime.of(2023,11,24,22,37))
+                .endEventDateTime(LocalDateTime.of(2023,11,23,22,37))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(this.objectMapper.writeValueAsString(eventDto))
+                )
+                .andExpect(status().isBadRequest());
+    }
 }
