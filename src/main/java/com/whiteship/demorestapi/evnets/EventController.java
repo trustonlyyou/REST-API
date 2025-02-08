f6d2878c549b671c23a0e56a6d68fda14f6f6f34
trushonlyyou <trushonlyyou@tnctec.co.kr>
2025-02-08 12:28:42 +0900

    Make Event Input Value Limits

--- src/main/java/com/whiteship/demorestapi/evnets/EventController.java
+++ src/main/java/com/whiteship/demorestapi/evnets/EventController.java
@@ -1,6 +1,7 @@
 package com.whiteship.demorestapi.evnets;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,9 +19,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 public class EventController {
 
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = eventRepository.save(event);
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createUri).body(event);
--- src/main/java/com/whiteship/demorestapi/evnets/EventDto.java
+++ src/main/java/com/whiteship/demorestapi/evnets/EventDto.java
@@ -1,19 +1,16 @@
 package com.whiteship.demorestapi.evnets;
 
-import lombok.*;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
-@Entity
-public class Event {
-
-    @Id @GeneratedValue
-    private Integer id;
+@Builder @NoArgsConstructor @AllArgsConstructor
+@Data
+public class EventDto {
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
@@ -24,9 +21,4 @@ public class Event {
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-    @Enumerated(EnumType.STRING)
-    private EventStatus eventStatus = EventStatus.DRAFT;
-
 }
